b88a7535d269fee2386e0e3128c1b26415eb5fca
Daniel Sjösten <dansjo_87@hotmail.com>
2016-10-04 13:24:08 +0200

    made some changes

--- Ex161004/src/Main.java
+++ Ex161004/src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hey!");
+		
+		System.out.println("Write Y:");
+
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		
+		
+		
+		if (input.equals("Y")) {
+			System.out.println("You made it");
+		}else{
+			System.out.println("You failed!!!");
+		}
 
 	}
 
